c4fba149b17cbf8f49a3199c1846b22716b2ff95
a635175 <a635175@DESKTOP-J1QQ4DF>
2024-10-23 11:36:17 +0530

    5h

--- Practice/src/RW/Happy.java
+++ Practice/src/RW/Happy.java
@@ -4,6 +4,12 @@ public class Happy {
 
 	public static void main(String[] args) {
 	System.out.println("Happy");
+	System.out.println("Happy");
+	System.out.println("Happy");
+	System.out.println("Happy");
+	System.out.println("Happy");
+	System.out.println("Happy");
+	
 
 	}
 
